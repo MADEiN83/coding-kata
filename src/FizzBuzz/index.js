0819893835c5db91f36d1c308a98345f6f56db2a
Anthony DI STEFANO <ADIS@supralog.local>
2020-06-04 11:25:04 +0200

    FizzBuzz completed

--- src/FizzBuzz/index.js
+++ src/FizzBuzz/index.js
@@ -1,5 +1,24 @@
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+
 class FizzBuzz {
-  proceed = () => {};
+  proceed = (number) => {
+    let output = "";
+
+    if (this.isDivisibleBy(number, 3) || this.contains(number, 3)) {
+      output += FIZZ;
+    }
+
+    if (this.isDivisibleBy(number, 5) || this.contains(number, 5)) {
+      output += BUZZ;
+    }
+
+    return output || number;
+  };
+
+  isDivisibleBy = (number, divider) => number % divider === 0;
+
+  contains = (number, wantedNumber) => number.toString().includes(wantedNumber);
 }
 
 export default FizzBuzz;
--- src/FizzBuzz/index.spec.js
+++ src/FizzBuzz/index.spec.js
@@ -1,6 +1,6 @@
 import FizzBuzz from ".";
 
-describe("", () => {
+describe("FizzBuzz tests", () => {
   let service;
 
   beforeEach(() => {
@@ -9,5 +9,29 @@ describe("", () => {
 
   it("should be defined after instanciation", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should returns '1' when '1' is provided", () => {
+    expect(service.proceed(1)).toBe(1);
+  });
+
+  it("should returns '2' when '2' is provided", () => {
+    expect(service.proceed(2)).toBe(2);
+  });
+
+  it("should returns 'Fizz' when number is divisible by '3'", () => {
+    expect(service.proceed(3)).toBe("Fizz");
+  });
+
+  it("should returns 'Buzz' when number is divisible by '5'", () => {
+    expect(service.proceed(10)).toBe("Buzz");
+  });
+
+  it("should returns 'FizzBuzz' when '15' is provided", () => {
+    expect(service.proceed(15)).toBe("FizzBuzz");
+  });
+
+  it("should returns 'FizzBuzz' when '53' is provided", () => {
+    expect(service.proceed(53)).toBe("FizzBuzz");
   });
 });
